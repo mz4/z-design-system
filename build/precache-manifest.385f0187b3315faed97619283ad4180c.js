e30180af445e49800343a9f0d0b94af24dd7c9c4
Matteo <matteozaratti@gmail.com>
2020-05-05 18:59:33 +0100

    Add gh-pages

---
+++ build/precache-manifest.385f0187b3315faed97619283ad4180c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4021b64e3f273c57d4a04d2291056b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe957464ee79b4f762ae",
+    "url": "/static/js/2.d807f579.chunk.js"
+  },
+  {
+    "revision": "fd16a4d6eee419f4c7961d5ddc9178e3",
+    "url": "/static/js/2.d807f579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec60ec6df3c4e54a7577",
+    "url": "/static/js/main.c81f5ea1.chunk.js"
+  },
+  {
+    "revision": "2bbc8f28d1801390d5c7",
+    "url": "/static/js/runtime-main.74a7afa9.js"
+  }
+]);
