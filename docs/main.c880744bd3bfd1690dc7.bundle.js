7f7e7b665dfc915b55b08b7dd15d8c59956580d4
Matteo <matteozaratti@gmail.com>
2020-05-10 17:00:09 +0100

    build docs

---
+++ docs/main.c880744bd3bfd1690dc7.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{473:function(n,o,p){p(474),p(617),p(1265),p(1270),p(1380),p(1382),n.exports=p(1416)},536:function(n,o){}},[[473,1,2]]]);
---
+++ docs/precache-manifest.20bf40c7a68c9a104c1fa2257d7d213e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603994a793f423dd11c469bb16637618",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dd749f672e2c730f93fb.bundle.js"
+  },
+  {
+    "url": "runtime~main.dd749f672e2c730f93fb.bundle.js"
+  },
+  {
+    "revision": "2127a77fa587390182627b33cdb8214c",
+    "url": "static/media/51603421.2127a77f.jpg"
+  },
+  {
+    "url": "vendors~main.dd749f672e2c730f93fb.bundle.js"
+  },
+  {
+    "url": "vendors~main.dd749f672e2c730f93fb.bundle.js.LICENSE.txt"
+  }
+]);
