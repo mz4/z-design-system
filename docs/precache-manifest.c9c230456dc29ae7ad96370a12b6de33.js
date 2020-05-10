09d0d96901a9ecff9ce7fc0865c3803af2a3445c
Matteo <matteozaratti@gmail.com>
2020-05-10 12:12:25 +0100

    docs folder

---
+++ docs/precache-manifest.c9c230456dc29ae7ad96370a12b6de33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884b18eec7984ce6cffbf747b55d40b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f2faad012170f2c703",
+    "url": "/static/js/2.ba2e20ed.chunk.js"
+  },
+  {
+    "revision": "fd16a4d6eee419f4c7961d5ddc9178e3",
+    "url": "/static/js/2.ba2e20ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be844b23f558096fa703",
+    "url": "/static/js/main.e0983799.chunk.js"
+  },
+  {
+    "revision": "4013707abf7af3d28ef9",
+    "url": "/static/js/runtime-main.3c2807ba.js"
+  },
+  {
+    "revision": "2127a77fa587390182627b33cdb8214c",
+    "url": "/static/media/51603421.2127a77f.jpg"
+  }
+]);
